0a34817614d96ca292c627e4cdf72868eecb52fa
Dens777 <dens77712@gmail.com>
2024-07-12 12:26:25 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,12 @@
+def devide(first,second):
+    res=0
+    if second==0:
+        return ('Ошибка')
+    else:
+        res=(first/second)
+        return res
+
+
+
+
+
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+def devide(first,second):
+    res=0
+    if second==0:
+        return inf
+    else:
+        res=(first/second)
+        return res
